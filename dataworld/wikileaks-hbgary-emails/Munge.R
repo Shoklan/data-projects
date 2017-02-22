1cb9b9cb63d4991715956e323c09ef6ee47a3e3a
Collin Mitchell <Shoklansenta@gmail.com>
2017-02-21 19:51:28 -0500

    working at last

--- dataworld/wikileaks-hbgary-emails/Munge.R
+++ dataworld/wikileaks-hbgary-emails/Munge.R
@@ -16,18 +16,18 @@
 # Notes:
 # delete objects to destory open connections.
 
-library(tidyverse)
+library( tidyverse )
 library( rvest )
 library( stringi )
-library(SOAR)
+library( SOAR )
+library( curl)
 
-Store()
 
 ## -- Varibales ---##
 filepath<- file.path("Code", "data-projects", "wikileaks-hbgary-emails")
 MAX_EMAIL_COUNT <- 71794
 url <- 'https://wikileaks.org/hbgary-emails/emailid/'
-
+urlSessionHome<- 'https://wikileaks.org'
 
 # TEST STUFF
 # urlhtml<- read_html(url)
@@ -40,13 +40,37 @@ url <- 'https://wikileaks.org/hbgary-emails/emailid/'
 # emailData<-  emailData[ which(emailData != "") ]
 
 ##-- Functions --#
+
 # Download the data and return a cleaned list
 wikileaksEmailScrape <- function( index, url){
   print( paste0("Reached count: ", index ))
   
+  
   completeUrl<- paste0(url, index)
-  urlhtml<- read_html(completeUrl)
-  emailText<- urlhtml %>%  html_nodes(".email-content") %>% html_text()
+  # urlhtml<- read_html(completeUrl)
+  
+  #while( !exists( quote( "urlhtml" ))){
+    # urlhtml<- read_html( curl(url = completeUrl, handle = curl::new_handle("useragent" = "Mozilla/5.0")))
+  #  urlhtml<- read_html( completeUrl, options = "NOERROR")
+    
+  #  Sys.sleep( 2 )
+  #  print( paste0( "Does urlhtml exists: ", exists(quote( "urlhtml" ))))
+  #}
+  
+  completedSuccessfully = FALSE
+  repeat({
+    completedSuccessfully <- tryCatch(
+      expr = {emailText <- sessionObject %>% 
+        jump_to( paste0(url, index) ) %>%
+        html_nodes(".email-content") %>%
+        html_text()},
+      error = function(e){ Sys.sleep(60); completedSuccessfully = FALSE}
+    )
+    print("loop")
+    if( completedSuccessfully != FALSE ) break
+  })
+  
+  # emailText<- urlhtml %>%  html_nodes(".email-content") %>% html_text()
   
   
   emailTemp<- unlist(strsplit( emailText, "\n"))
@@ -54,9 +78,14 @@ wikileaksEmailScrape <- function( index, url){
   emailData<- unlist(map(emailTemp,  gsub, pattern = "^>|\t|\n", replacement = ""))
   emailData<- unlist(map( emailData, stri_trim))
   emailData<-  emailData[ which(emailData != "") ]
-  rm(urlhtml)
-  gc()
+  
   emailData
+}
+
+# Return a session object
+getSession<- function( urlTarget ){
+  sessionObject <- html_session( urlTarget )
+  sessionObject
 }
 
 # Test function
@@ -64,6 +93,10 @@ wikileaksEmailScrape <- function( index, url){
 
 
 ##-- Main --##
+Store()
+
+sessionObject<- getSession( urlSessionHome )
+
 collectedEmails <- safely(map( 1:MAX_EMAIL_COUNT, wikileaksEmailScrape, url = url))
 Store( collectedEmails )
 
