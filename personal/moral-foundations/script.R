e42fd8b09e5ed95460a31feda5f449177a00deae
Collin Mitchell <Shoklansenta@gmail.com>
2018-02-03 18:27:22 -0500

    Created sample function
    .

--- personal/moral-foundations/script.R
+++ personal/moral-foundations/script.R
@@ -11,27 +11,82 @@ library( iotools )
 library( rvest )
 
 ##-- Functions
+######
+
+# Iterate over and download all the files
+downloadDocuments <- function( docNumbers, urlBase ){
+  walk2( .x = docNumbers, .y = docNumbers, .f = function(.x, .y){
+    urlTarget<- sprintf( urlBase, .x, .y)
+    urlPieces <- str_split(urlTarget, "/") %>% unlist()
+    filename<- urlPieces[ length( urlPieces)]
+    
+    download.file( url = urlTarget, destfile = file.path('data', filename), mode = "wb" )
+  })
+} # END downloadDocuments
+
+
+# iterate over document chunks and join sentiment data
+combineSentimentDocuments <- function(chunk){
+  x <- dstrsplit( chunk, col_types = 'character', sep = ',')
+  x$V2 <- NULL
+  sentimentFrame<- inner_join(x, nrcFrame, by = c('V1' = 'word') )
+  sentimentFrame$count <- 1
+  sentimentFrame
+} # END combineSentimentDocuments
+
+
+# chunk apply over data and return sentiments per term.
+collectTermsList<- function( cores = 4){
+  # openfile connection
+  fc <- file('data/terms.output', 'rb')
+  
+  # iterate over chunks
+  termsList <- chunk.apply( fc, combineSentimentDocuments, CH.MAX.SIZE = 1e6, parallel = cores)
+  
+  # close connection
+  close( fc )
+  
+  
+  # return data
+  colnames( termsList) <- c('terms', 'sentiment', 'count')
+  termsList
+} # END collectTermsList
+
+
+# sample the data and return a ggplot object
+sampleData <- function( completeDataset, sampleSize = 75000 ){
+  completeDataset %>%
+    sample_n( sampleSize ) %>%
+    filter(!sentiment %in% c("positive", "negative")) %>%
+    ggplot(aes( sentiment, fill = sentiment )) + geom_bar() + ggtitle('Voltaire\'s Emotional Content' )
+}
 
 
 
+######
+##-- Variables
+######
 
-  ## Download the files.
+# download link for voltaire docuemnts.
 urlBase <- "https://ia801406.us.archive.org/10/items/worksofvoltairec%02dvolt/worksofvoltairec%02dvolt.pdf"
 
-  # The file iterates.  
-volumeNumbers<- seq(3, 22, 1)
-# volumeNumbers<- seq(3, 3, 1)
+# iterate through with voltaire documents
+documentNumbers<- seq(3, 22, 1)
 
-  # Iterate over and download all the files
-walk2( .x = volumeNumbers, .y = volumeNumbers, .f = function(.x, .y){
-  urlTarget<- sprintf( urlBase, .x, .y)
-  urlPieces <- str_split(urlTarget, "/") %>% unlist()
-  filename<- urlPieces[ length( urlPieces)]
-  
-  download.file( url = urlTarget, destfile = file.path('data', filename), mode = "wb" )
-})
 
-## -- End DOWNLOAD FILES 
+#########
+##-- MAIN
+#########
+
+# get plutchik's sentiments
+nrcFrame<- get_sentiments('nrc')
+
+# download documents
+downloadDocuments( documentNumbers, urlBase )
+
+termsList <- collectTermsList()
+
+
 
 
 
@@ -39,26 +94,11 @@ walk2( .x = volumeNumbers, .y = volumeNumbers, .f = function(.x, .y){
 ##--- PLAY 
 
 # load the data
-data<- read_csv('terms.output', col_names = FALSE, progress = FALSE)[,1]
+data<- read_csv('data/terms.output', col_names = FALSE, progress = FALSE)[,1]
 colnames( data ) <- c('terms')
 
-nrcFrame<- get_sentiments('nrc')
 
-play <- function(chunk){
-  x <- dstrsplit( chunk, col_types = 'character', sep = ',')
-  x$V2 <- NULL
-  sentimentFrame<- inner_join(x, sentimentFrame, by = c('V1' = 'word') )
-  sentimentFrame$count <- 1
-  sentimentFrame
-}
 
-fc <- file('terms.output', 'rb')
-
-termsList <- chunk.apply( fc, play, CH.MAX.SIZE = 1e6, parallel = 4)
-close( fc )
-termsList
-
-colnames( termsList) <- c('terms', 'sentiment')
 
 results <- termsList %>%
   # Only consider Plutchik sentiments
@@ -72,18 +112,5 @@ termsList %>%
   # Only consider Plutchik sentiments
   filter(!sentiment %in% c("positive", "negative")) %>%
   ggplot(aes( sentiment, fill = sentiment )) + geom_bar() + ggtitle('Voltaire\'s Emotional Content' )
-
-
-
-
-temp<- getSession( "https://archive.org/")
-
-tmp<- temp %>%
-  jump_to( urlTemp ) %>%
-  follow_link('PDF')
-  
-  
-  
-  
   
   
