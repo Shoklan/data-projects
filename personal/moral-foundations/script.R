56c0cdad8632291d674bd1110d52f2d8265aecb3
Collin Mitchell <Shoklansenta@gmail.com>
2018-02-10 10:16:31 -0500

    passing to laptop
    .

--- personal/moral-foundations/script.R
+++ personal/moral-foundations/script.R
@@ -2,19 +2,15 @@
 # Date: 2018/1/28
 # Purpose: Sentiment Analysis of Voltaire, Burke, Paine
 
-library( stringr )
 library( tidytext )
 library( tidyverse )
-library( qdap )
 library( iotools )
-
-library( rvest )
 
 ##-- Functions
 ######
 
 # Iterate over and download all the files
-downloadDocuments <- function( docNumbers, urlBase ){
+downloadDocuments <- function( docNumbers, urlBase){
   walk2( .x = docNumbers, .y = docNumbers, .f = function(.x, .y){
     urlTarget<- sprintf( urlBase, .x, .y)
     urlPieces <- str_split(urlTarget, "/") %>% unlist()
@@ -22,13 +18,25 @@ downloadDocuments <- function( docNumbers, urlBase ){
     
     download.file( url = urlTarget, destfile = file.path('data', filename), mode = "wb" )
   })
-} # END downloadDocuments
+} # END downloadVoltaireDocuments
+
+
+
+
+# clean the work directory
+clearDirectory<- function(){
+  walk( file.path( 'data', list.files( 'data')), file.remove)
+}
+
+
+
 
 
 # iterate over document chunks and join sentiment data
 combineSentimentDocuments <- function(chunk){
   x <- dstrsplit( chunk, col_types = 'character', sep = ',')
   x$V2 <- NULL
+  x<- tolower(x)
   sentimentFrame<- inner_join(x, nrcFrame, by = c('V1' = 'word') )
   sentimentFrame$count <- 1
   sentimentFrame
@@ -46,7 +54,6 @@ collectTermsList<- function( cores = 4){
   # close connection
   close( fc )
   
-  
   # return data
   colnames( termsList) <- c('terms', 'sentiment', 'count')
   termsList
@@ -54,12 +61,37 @@ collectTermsList<- function( cores = 4){
 
 
 # sample the data and return a ggplot object
-sampleData <- function( completeDataset, sampleSize = 75000 ){
+sampleData <- function( completeDataset, sampleSize = 75000, graphTitle ){
   completeDataset %>%
     sample_n( sampleSize ) %>%
     filter(!sentiment %in% c("positive", "negative")) %>%
-    ggplot(aes( sentiment, fill = sentiment )) + geom_bar() + ggtitle('Voltaire\'s Emotional Content' )
-}
+    ggplot(aes( sentiment, fill = sentiment )) + geom_bar() + ggtitle( graphTitle )
+} # END sampleData
+
+
+
+# 
+combinePaineSentimentDocs <- function( .x ){
+  textData<- readLines( .x )
+  
+  tempText<- textData %>%
+    str_split(' ') %>%
+    unlist() %>%
+    map( str_replace_all, pattern = '\\W', replacement = '' ) %>%
+    unlist() %>%
+    as_tibble() %>%
+    tail(., nrow(.) - 61 ) %>%
+    mutate(count = 1)
+  
+  colnames( tempText ) <- c('terms', 'count')
+  
+  result<-tempText %>%
+    inner_join( nrcFrame, by = c('terms' = 'word') ) %>%
+    # Only consider Plutchik sentiments
+    filter(!sentiment %in% c("positive", "negative"))
+  
+  result
+} # END combinePaineSentimentDocs
 
 
 
@@ -68,10 +100,19 @@ sampleData <- function( completeDataset, sampleSize = 75000 ){
 ######
 
 # download link for voltaire docuemnts.
-urlBase <- "https://ia801406.us.archive.org/10/items/worksofvoltairec%02dvolt/worksofvoltairec%02dvolt.pdf"
+urlVoltaireBase <- "https://ia801406.us.archive.org/10/items/worksofvoltairec%02dvolt/worksofvoltairec%02dvolt.pdf"
+urlPaineBase<- "http://www.gutenberg.org/cache/epub/374%d/pg374%d.txt"
+
 
 # iterate through with voltaire documents
-documentNumbers<- seq(3, 22, 1)
+voltaireDocumentNumbers<- seq(3, 22, 1)
+
+# iterate through with paine documents
+paineDocumentNumbers<- 1:3
+
+
+
+
 
 
 #########
@@ -81,10 +122,29 @@ documentNumbers<- seq(3, 22, 1)
 # get plutchik's sentiments
 nrcFrame<- get_sentiments('nrc')
 
-# download documents
-downloadDocuments( documentNumbers, urlBase )
+## Voltaire documents
+# download voltaires
+downloadDocuments( voltaireDocumentNumbers, urlVoltaireBase )
 
+# create sentiment frame for analysis
 termsList <- collectTermsList()
+
+# generate Voltaire's Sentiments.
+voltaireGraph <- sampleData( termsList, graphTitle = "Voltaire's Sentiments" )
+## END VOLTAIRE
+
+
+## Thomas Paine documents
+downloadDocuments( paineDocumentNumbers, urlPaineBase )
+
+files<- file.path( 'data', list.files('data'))
+
+termsList<- map( files, combinePaineSentimentDocs ) %>% bind_rows()
+paineGraph <- sampleData( termsList, sampleSize = 10000, graphTitle = "Thomas Paine's Sentiments")
+
+paineGraph
+
+
 
 
 
@@ -96,6 +156,12 @@ termsList <- collectTermsList()
 # load the data
 data<- read_csv('data/terms.output', col_names = FALSE, progress = FALSE)[,1]
 colnames( data ) <- c('terms')
+
+
+
+
+
+
 
 
 
