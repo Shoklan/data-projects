2a5679890ef9bb1cc485c0679e920dbd377fdbd6
Collin Mitchell <Shoklansenta@gmail.com>
2018-02-21 20:57:31 -0500

    Starting Russian Bots Analysis
    init

---
+++ personal/russian-bots/munge.R
@@ -0,0 +1,25 @@
+# Author: Collin Mitchell
+#  Date: 2018/02/21
+# Purpose Explore 'Russian Bots' Dataset
+
+##-- Libraries 
+library( tidyverse )
+library( stringr )
+library( tidytext )
+
+##-- Variables
+directory  <- 'data'
+sampleName <- 'sample.csv'
+tweetsName <- 'tweets.csv'
+usersName  <- 'users.csv'
+
+
+##-- Functions
+readData<- 
+
+
+##-- Main
+
+
+
+##-- Play
